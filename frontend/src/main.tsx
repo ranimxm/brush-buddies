befb1bacb2f0f40449c9036cd67affaa17e96136
Ranim <i502848@fhict.nl>
2024-07-03 21:01:02 +0200

    adding login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import "./styles/index.css";
+import { muiTheme } from "./styles/mui-theme";
+import { ThemeProvider } from "@emotion/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <HelmetProvider>
-        <App />
+        <ThemeProvider theme={muiTheme}>
+          <App />
+        </ThemeProvider>
       </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>,
